b8a196ce62a1fdcf878a5649d7b7db160a5d152c
kobe2403 <kobe2403.momo@gmail.com>
2021-03-17 23:52:37 +0900

    3-4章のフフォーム送信

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -4,59 +4,55 @@ import "./App.css"
 import Rect from "./components/Rect"
 
 
-class App extends Component<any, {}> {
-    input = "";
+class App extends Component<any, {title:string, message:string}> {
+    input:string = "";
 
     constructor(props:any) {
-        super(props)
+        super(props);
+        this.state = {
+            title: "input form",
+            message: "type your name.",
+        }
+        this.doChange = this.doChange.bind(this);
+        this.doSubmit = this.doSubmit.bind(this);
+    }
+
+    //入力値の変更判定
+    doChange(event:any) {
+        this.input = event.target.value;
+    }
+
+    doSubmit(event:any){
+        this.setState({
+            title: "send form",
+            message: "Hello," + this.input + "!!",
+        })
+        event.preventDefault();//フォーム送信の無効化
     }
 
     render() {
         return <div>
             <h1 className="bg-primary text-white display-4">React</h1>
             <div className="container">
-                <Message title="Children!!" >
-                    あああああああああああああああ。
-                    あああああああああああああああ。
-                    あああああああああああああああ。
-                </Message>
+               <h4>{this.state.title}</h4>
+               <p className="card h5 p-3">{this.state.message}</p>
+               <div className="alert alert-primary mt-3">
+                   <form onSubmit={this.doSubmit}>{/** onSubmitはhtmlに備わっているフォーム送信処理*/}
+                       <div className="form-group">
+                            <label>Message:</label>
+                            {/** 
+                             * onChangeはhtmlに備わっている入力値変更判定処理
+                             * required patternでバリデーションチェック
+                            */}
+                            <input type="text" className="form-control" 
+                                onChange={this.doChange} required pattern="[A-Za-z _,.]+" />
+                        </div>
+                        <input type="submit" className="btn btn-primary" value="Click"></input>
+                   </form>
+               </div>
             </div>
         </div>
     }
 }
-
-class Message extends Component<{title:string}, {value:string, key:string}> {
-    li:object = {
-        fontSize: "14pt",
-        fontWeight: "bold",
-        color: "#090",
-    }
-
-    render() {
-        let content:any = this.props.children;
-        let arr:any;
-        if (content){
-            arr = content.split("。");
-        }
-        let arr2:any = [];
-        for (let i = 0; i < arr.length; i++){
-            if (arr[i].trim() != ""){
-                arr2.push(arr[i]);
-            }
-        }
-        let list = arr2.map((value:string, key:string) =>(
-            <li className="list-group-item" style={this.li}
-                key={key}>
-                    {key + 1} . {value}.
-            </li>
-        ));
-
-        return <div>
-            <h2>{this.props.title}</h2>
-            <ol className="list-group">{list}</ol>
-        </div>
-    }
-}
-
 
 export default App
