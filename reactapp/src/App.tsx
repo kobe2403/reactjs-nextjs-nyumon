9a450cd0eb445c15e7d08e1913a8368c146fb06f
kobe2403 <kobe2403.momo@gmail.com>
2021-03-18 14:33:48 +0900

    3-4章コンポーネントイベントの作成

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css"
 import Rect from "./components/Rect"
 
 
-class App extends Component<any, {title:string, message:string}> {
+class App extends Component<any, {title:string, message:string, max: number}> {
     input:string = "";
 
     constructor(props:any) {
@@ -12,9 +12,11 @@ class App extends Component<any, {title:string, message:string}> {
         this.state = {
             title: "input form",
             message: "type your name.",
+            max: 10,
         }
-        this.doChange = this.doChange.bind(this);
-        this.doSubmit = this.doSubmit.bind(this);
+        /* this.doChange = this.doChange.bind(this);
+        this.doSubmit = this.doSubmit.bind(this); */
+        this.doCheck = this.doCheck.bind(this);
     }
 
     //入力値の変更判定
@@ -30,29 +32,48 @@ class App extends Component<any, {title:string, message:string}> {
         event.preventDefault();//フォーム送信の無効化
     }
 
+    //バリデーションチェック
+    doCheck(e:any) {
+        alert(e.target.value + "は長すぎます。（最大" + this.state.max + "文字）")
+    }
+
     render() {
         return <div>
             <h1 className="bg-primary text-white display-4">React</h1>
             <div className="container">
                <h4>{this.state.title}</h4>
-               <p className="card h5 p-3">{this.state.message}</p>
-               <div className="alert alert-primary mt-3">
-                   <form onSubmit={this.doSubmit}>{/** onSubmitはhtmlに備わっているフォーム送信処理*/}
-                       <div className="form-group">
-                            <label>Message:</label>
-                            {/** 
-                             * onChangeはhtmlに備わっている入力値変更判定処理
-                             * required patternでバリデーションチェック
-                            */}
-                            <input type="text" className="form-control" 
-                                onChange={this.doChange} required pattern="[A-Za-z _,.]+" />
-                        </div>
-                        <input type="submit" className="btn btn-primary" value="Click"></input>
-                   </form>
-               </div>
+               <Message maxlength={this.state.max} onCheck={this.doCheck} />
             </div>
         </div>
     }
+}
+
+class Message extends Component<{maxlength:number, onCheck:any}, {}> {
+    li:object = {
+        fontSize: "14pt",
+        fontWeight: "bold",
+        color: "#090",
+    }
+
+    constructor(props:any) {
+        super(props);//親クラスののコンストラクタを指定
+        this.doChange = this.doChange.bind(this);
+    }
+
+    doChange(e:any) {
+        if (e.target.value.length > this.props.maxlength){
+            this.props.onCheck(e);
+            e.target.value = e.target.value.substr(0, this.props.maxlength)
+        }
+    }
+
+    render() {
+        return <div className="form-group">
+            <label>input:</label>
+            <input type="text" className="form-control" onChange={this.doChange} />
+        </div>
+    }
+
 }
 
 export default App
