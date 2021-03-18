1aab89dbcf938f65ab5937c1ac8f5447eb05c2f4
kobe2403 <kobe2403.momo@gmail.com>
2021-03-18 14:53:26 +0900

    コンテキストの使用

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -3,77 +3,44 @@ import React, {Component} from "react"
 import "./App.css"
 import Rect from "./components/Rect"
 
+let data = {
+    title: "React-context",
+    message: "this is sample message.",
+};
 
-class App extends Component<any, {title:string, message:string, max: number}> {
-    input:string = "";
+const SampleContext = React.createContext(data);
 
-    constructor(props:any) {
-        super(props);
-        this.state = {
-            title: "input form",
-            message: "type your name.",
-            max: 10,
-        }
-        /* this.doChange = this.doChange.bind(this);
-        this.doSubmit = this.doSubmit.bind(this); */
-        this.doCheck = this.doCheck.bind(this);
-    }
-
-    //入力値の変更判定
-    doChange(event:any) {
-        this.input = event.target.value;
-    }
-
-    doSubmit(event:any){
-        this.setState({
-            title: "send form",
-            message: "Hello," + this.input + "!!",
-        })
-        event.preventDefault();//フォーム送信の無効化
-    }
-
-    //バリデーションチェック
-    doCheck(e:any) {
-        alert(e.target.value + "は長すぎます。（最大" + this.state.max + "文字）")
-    }
-
+class App extends Component<{}, {}> {
     render() {
         return <div>
             <h1 className="bg-primary text-white display-4">React</h1>
             <div className="container">
-               <h4>{this.state.title}</h4>
-               <Message maxlength={this.state.max} onCheck={this.doCheck} />
+                <Title />
+                <Message />
             </div>
         </div>
     }
 }
 
-class Message extends Component<{maxlength:number, onCheck:any}, {}> {
-    li:object = {
-        fontSize: "14pt",
-        fontWeight: "bold",
-        color: "#090",
-    }
-
-    constructor(props:any) {
-        super(props);//親クラスののコンストラクタを指定
-        this.doChange = this.doChange.bind(this);
-    }
-
-    doChange(e:any) {
-        if (e.target.value.length > this.props.maxlength){
-            this.props.onCheck(e);
-            e.target.value = e.target.value.substr(0, this.props.maxlength)
-        }
-    }
+class Title extends Component<{}, {}> {
+    static contextType = SampleContext;
 
     render() {
-        return <div className="form-group">
-            <label>input:</label>
-            <input type="text" className="form-control" onChange={this.doChange} />
+        return <div className="card my-3">
+            <h2>{this.context.title}</h2>
         </div>
     }
-
 }
+
+class Message extends Component<{}, {}> {
+    static contextType = SampleContext;
+
+    render() {
+        return <div className="alert alert-primary">
+            <h2>{this.context.message}</h2>
+        </div>
+    }
+}
+
 
 export default App
