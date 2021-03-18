fcd233edd389f51eedcb14896599020cf4abd2fe
kobe2403 <kobe2403.momo@gmail.com>
2021-03-18 23:12:22 +0900

    属性の更新

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -2,16 +2,24 @@ import { render } from "@testing-library/react";
 import React, {useState} from "react"
 import "./App.css"
 
+function AlertMessage(props:any) {
+    return <div className="alert alert-primary h5 text-primary">
+        {props.message}
+    </div>
+}
+
+function CardMessage(props:any) {
+    return <div className="card p-3 h5 border-primary text-center">
+        {props.message}
+    </div>
+}
 
 function App() {
-    const [count, setCount] = useState(0)
-    const clickFunk = () => {
-        setCount(count + 1)
-    }
+    const [msg,setMsg] = useState("This is sample message!!");
 
-    const [flg, setFlag] = useState(false)
-    const changeflag = (e:any) => {
-        setFlag(e.target.checked)
+    const doAction = () => {
+        let res = window.prompt("type your name:");
+        setMsg("Hello" + res + "!!");
     }
 
     return (
@@ -19,26 +27,10 @@ function App() {
             <h1 className="bg-primary text-white display-4">React</h1>
             <div className="container">
                 <h4 className="my-3">Hooks sample</h4>
-                {flg ? 
-                <div className="alert alert-primary text-center">
-                    <p className="h5 mb-3">click: {count} times!</p>
-                    <div>
-                    <button className="btn btn-primary" onClick={clickFunk}>Click here!</button>
-                    </div>
-                </div>
-                :
-                <div className="card p-3 border-primary text-center">
-                    <p className="h5 mb-3 text-left text-primary">click: {count} times!</p>
-                    <div>
-                    <button className="btn btn-primary" onClick={clickFunk}>Click here!</button>
-                    </div>
-                </div>
-                }
-                <div className="form-group h6 text-center pt-3">
-                    <input type="checkbox" className="form-check-input" id="check1" onChange={changeflag}></input>
-                    <label className="form-check-label" htmlFor="check1">
-                        Change form style.
-                    </label>
+                <AlertMessage message={msg} />
+                <CardMessage message={msg} />
+                <div className="text-center">
+                    <button onClick={doAction} className="btn btn-primary">click</button>
                 </div>
             </div>
         </div>
