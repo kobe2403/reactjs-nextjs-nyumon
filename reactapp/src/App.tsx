c329aef48be4f84b0ab812a2d2aab89b4b9146d8
kobe2403 <kobe2403.momo@gmail.com>
2021-03-18 15:40:29 +0900

    複数ステートフックの例

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,58 +1,48 @@
 import { render } from "@testing-library/react";
-import React, {Component} from "react"
+import React, {useState} from "react"
 import "./App.css"
 
-let data = {
-    title: "React-context",
-    message: "this is sample message.",
-};
 
-const SampleContext = React.createContext(data);
-
-class App extends Component<{}, {}> {
-    newdata = {
-        title: "new title",
-        message: "new message",
+function App() {
+    const [count, setCount] = useState(0)
+    const clickFunk = () => {
+        setCount(count + 1)
     }
 
-    render() {
-        return <div>
+    const [flg, setFlag] = useState(false)
+    const changeflag = (e:any) => {
+        setFlag(e.target.checked)
+    }
+
+    return (
+        <div>
             <h1 className="bg-primary text-white display-4">React</h1>
             <div className="container">
-                <Title />
-                <Message />
-                <hr />
-                <SampleContext.Provider value={this.newdata}>
-                    <Title />
-                    <Message />
-                </SampleContext.Provider>
-                <hr />
-                <Title />
-                <Message />
+                <h4 className="my-3">Hooks sample</h4>
+                {flg ? 
+                <div className="alert alert-primary text-center">
+                    <p className="h5 mb-3">click: {count} times!</p>
+                    <div>
+                    <button className="btn btn-primary" onClick={clickFunk}>Click here!</button>
+                    </div>
+                </div>
+                :
+                <div className="card p-3 border-primary text-center">
+                    <p className="h5 mb-3 text-left text-primary">click: {count} times!</p>
+                    <div>
+                    <button className="btn btn-primary" onClick={clickFunk}>Click here!</button>
+                    </div>
+                </div>
+                }
+                <div className="form-group h6 text-center pt-3">
+                    <input type="checkbox" className="form-check-input" id="check1" onChange={changeflag}></input>
+                    <label className="form-check-label" htmlFor="check1">
+                        Change form style.
+                    </label>
+                </div>
             </div>
         </div>
-    }
+    )
 }
-
-class Title extends Component<{}, {}> {
-    static contextType = SampleContext;
-
-    render() {
-        return <div className="card my-3">
-            <h2>{this.context.title}</h2>
-        </div>
-    }
-}
-
-class Message extends Component<{}, {}> {
-    static contextType = SampleContext;
-
-    render() {
-        return <div className="alert alert-primary">
-            <h2>{this.context.message}</h2>
-        </div>
-    }
-}
-
 
 export default App
