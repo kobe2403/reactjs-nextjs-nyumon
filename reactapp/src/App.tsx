90b801f23234ddd05ad10d4358ca00b79cb0f2d1
kobe2403 <kobe2403.momo@gmail.com>
2021-03-21 21:20:37 +0900

    更新

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -2,32 +2,40 @@ import { render } from "@testing-library/react";
 import React, {useEffect, useState} from "react"
 import "./App.css"
 
-/**
- * 独自ステート
- * use〇〇で勝手にフックだと判断してくれる
- * const [変数, 関数] = useState(初期値);
- * const 関数
- * return 値
- */
-function useCounter() {
-    const [num, setNum] = useState(0);//初期値は0、変数はnum、変更は関数setNumで行う。
-    //独自フックの関数
-    const count = () => {
-        setNum(num + 1);//ステートの変数numに＋１する。これをsetNum関数でnumに上書き
-    }
+//フック
+const useTax = (t1:number, t2:number) => {
+    const [price, setPrice]= useState(100);//ステート
+    const [tx1] = useState(t1);//ステート。税率
+    const [tx2] = useState(t2);//ステート。税率
 
-    return [num, count];//ステートの値を得るものnumと数字を1増やす関数countを返す。
-}
+    //型定義入れないとエラー発生
+    const tax:any = () => {
+        return Math.floor(price * (1.0 + tx1 / 100));
+    };//関数
+    //型定義入れないとエラー発生
+    const reduced:any = () => {
+        return Math.floor(price * (1.0 + tx2 / 100));
+    };//関数
+    return [price, tax, reduced, setPrice];//戻り値
+};
 
 function AlertMessage() {
-    const [counter, plus] = useCounter();
+    const [price, tax, reduced, setPrice] = useTax(10, 8);
+    const DoChange = (e:any) => {
+        let p = e.target.value;
+        setPrice(p);
+    };
+
     return (
-        <div className="alert alert-primary h5 text-center">
-            <h4>Count:{counter}</h4>
-            {/**plusの型をanyで対応 */}
-            <button onClick={plus as any} className="btn btn-primary">count</button>
+        <div className="alert alert-primary h5">
+            <p className="h5">通常税率：{tax()}円.</p>
+            <p className="h5">軽減税率：{reduced()}円.</p>
+            <div className="form-group">
+                <label className="form-group-label">Price:</label>
+                <input type="number" className="form-control" onChange={DoChange} value={price as any} />
+            </div>
         </div>
-    )
+    );
 }
 
 function App() {
