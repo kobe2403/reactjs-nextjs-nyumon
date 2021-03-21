e225f8989781a0e3a2df12e24a86772d11562e61
kobe2403 <kobe2403.momo@gmail.com>
2021-03-21 21:57:51 +0900

    独自フック完了

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -2,38 +2,74 @@ import { render } from "@testing-library/react";
 import React, {useEffect, useState} from "react"
 import "./App.css"
 
-//フック
-const useTax = (t1:number, t2:number) => {
-    const [price, setPrice]= useState(100);//ステート
-    const [tx1] = useState(t1);//ステート。税率
-    const [tx2] = useState(t2);//ステート。税率
-
-    //型定義入れないとエラー発生
-    const tax:any = () => {
-        return Math.floor(price * (1.0 + tx1 / 100));
-    };//関数
-    //型定義入れないとエラー発生
-    const reduced:any = () => {
-        return Math.floor(price * (1.0 + tx2 / 100));
-    };//関数
-    return [price, tax, reduced, setPrice];//戻り値
+const total = (val:any) => {
+    let re = 0;
+    for (let i = 0; i < val; i++){
+        re += i;
+    }
+    return re;
 };
 
+//消費税の計算
+const tax = (val:any) => {
+    return Math.floor(val * 1.1);
+}
+
+//数値を計算し、メッセージを返す独自フック
+const useCalc = (num = 0, func = (val:any) => {return val}) => {
+    const [msg, setMsg]:[string, any] = useState("");
+    const P = num;
+    const setValue = (p:any) => {
+        let res = func(p);
+        setMsg(<p className="h5">※{p}の結果は、{res}です。</p>);
+    };
+
+    return [msg, setValue];
+};
+
+//デフォルトコンポーネント
+function PlainMessage() {
+    const [msg, setCalc]:any[] = useCalc(0);
+    const onChange = (e:any) => {
+        setCalc(e.target.value);
+    };
+
+    return (
+        <div className="p-3 h5">
+            <h5>{msg}</h5>
+            <input type="number" onChange={onChange} className="form-control" />
+        </div>
+    );
+
+}
+
+//合計計算コンポーネント
 function AlertMessage() {
-    const [price, tax, reduced, setPrice] = useTax(10, 8);
-    const DoChange = (e:any) => {
+    const [msg, setCalc]:any[] = useCalc(0 ,total);
+    const onChange = (e:any) => {
         let p = e.target.value;
-        setPrice(p);
+        setCalc(p);
     };
 
     return (
         <div className="alert alert-primary h5">
-            <p className="h5">通常税率：{tax()}円.</p>
-            <p className="h5">軽減税率：{reduced()}円.</p>
-            <div className="form-group">
-                <label className="form-group-label">Price:</label>
-                <input type="number" className="form-control" onChange={DoChange} value={price as any} />
-            </div>
+            <p className="h5">{msg}</p>
+            <input type="number" className="form-control" onChange={onChange} min="0" max="10000" />
+        </div>
+    );
+}
+
+//消費税計算コンポーネント
+function CardMessage() {
+    const [msg, setCalc]:any[] = useCalc(0, tax);
+    const onChange = (e:any)=> {
+        setCalc(e.target.value);
+    }
+
+    return (
+        <div className="card p-3 h5 border-primary">
+            <h5>{msg}</h5>
+            <input type="range" onChange={onChange} min="0" max="10000" step="100" className="from-control" />
         </div>
     );
 }
@@ -44,7 +80,9 @@ function App() {
             <h1 className="bg-primary text-white display-4">React</h1>
             <div className="container">
                 <h4 className="my-3">Hooks sample</h4>
+                <PlainMessage />
                 <AlertMessage />
+                <CardMessage />
             </div>
         </div>
     )
